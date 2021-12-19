9d4a1b6cd4e79d0ea25dd58c0b0548e8c2c1cd57
Andrew Read <aj.read13@gmail.com>
2021-12-19 15:03:34 -0500

    working on fetch

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,26 @@ var apiKey = "69b306c955b2b853b681d7ca8dfd46f3";
 var cityName = document.querySelector("#cityInput");
 var citySearchButton = document.querySelector("#citySearchButton");
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+var apiURL = "https://api.openweather.org/data/2.5/weather?q=" + cityName + ""
+var
+
+
+
+var getWeather = function (cityChoice) {
+    fetch(apiURL)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    displayWeather(data, cityChoice);
+                });
+            else {
+                alert("Error:  " + response.statusText)
+            }
+        .catch(function (error) {
+                alert(" Unable to Connect to Weather Server");
+            }
+            )
+        }
+    }
+
+}

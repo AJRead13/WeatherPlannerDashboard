36db3648d682dba32617f5687a6ae2a935495298
Andrew Read <aj.read13@gmail.com>
2021-12-19 13:32:45 -0500

    basic html structure, api key and some variables

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,4 @@
-var apiKey = "69b306c955b2b853b681d7ca8dfd46f3"
+var apiKey = "69b306c955b2b853b681d7ca8dfd46f3";
+var cityName = document.querySelector("#cityInput");
+var citySearchButton = document.querySelector("#citySearchButton");
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}

ff9a545bbb67cbc4b0f7bdc95872b74de034756e
Andrew Read <aj.read13@gmail.com>
2021-12-19 20:33:23 -0500

    added parts to script for localstorage and attempting to get api to work, need to get api to work before i can build out rest of script for dynamically building elements with forcasts

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,26 +2,58 @@ var apiKey = "69b306c955b2b853b681d7ca8dfd46f3";
 var cityName = document.querySelector("#cityInput");
 var citySearchButton = document.querySelector("#citySearchButton");
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-var apiURL = "https://api.openweather.org/data/2.5/weather?q=" + cityName + ""
-var
+//var apiURL = "https://api.openweather.org/data/2.5/weather?q=" + cityName + "appid=" + apiKey
+var citySearchReturn = document.querySelector("#citySearchReturn");
 
 
 
-var getWeather = function (cityChoice) {
-    fetch(apiURL)
-        .then(function (response) {
-            if (response.ok) {
-                response.json().then(function (data) {
-                    displayWeather(data, cityChoice);
-                });
-            else {
-                alert("Error:  " + response.statusText)
-            }
-        .catch(function (error) {
-                alert(" Unable to Connect to Weather Server");
-            }
-            )
-        }
+function returnCities() {
+    localStorage.getItem(savedCity, cityName)
+    if (null){
+        return;
     }
+};
 
+var getWeather = function (cityName) {
+    var apiURL = "https://api.openweather.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
+  
+    fetch(apiURL)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            displayWeather(data, cityName);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to Weather');
+      });
+  };
+
+//localStorage.setItem(savedCity, cityName)
+
+var showForcast = function (weather, cityWeather) {
+    if (weather.length === 0) {
+        citySearchReturn.textContent = "No Weather Data Available";
+        return;
+    }
+    cityName.textContent = city;    
+
+    for (var i = 0; i < weather.length; i++ ) {
+        var weatherDay = weather[i].data
+        var forcastDay = document.createElement("div");
+        forcastDay.classList = "";
+        forcastDay.setAttribute("id", );
+
+    }
 }
+
+
+citySearchButton.addEventListener("submit", getWeather())
+
+
+//var savedCities = document.createElement("button");
+
+

97ad773add1e76e61b188cbf297e8b72f6eb9301
Andrew Read <aj.read13@gmail.com>
2021-12-20 17:35:02 -0500

    got local storage working, got api working, need to get data to render

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,10 +1,13 @@
 var apiKey = "69b306c955b2b853b681d7ca8dfd46f3";
-var cityName = document.querySelector("#cityInput");
-var citySearchButton = document.querySelector("#citySearchButton");
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-//var apiURL = "https://api.openweather.org/data/2.5/weather?q=" + cityName + "appid=" + apiKey
-var citySearchReturn = document.querySelector("#citySearchReturn");
+//var cityName = document.querySelector("#cityInput");
+var citySearchButton = document.querySelector("#citySearchButton"); 
+var cityList = document.querySelector("#previousCities");
+var cityArray = JSON.parse(localStorage.getItem("history")) || []
 
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "appid=" + apiKey
+var citySearchReturn = document.querySelector("#citySearchReturn");
+//var tempEl = (response.main.temp - 273.15) * 1.8 + 32;
 
 
 function returnCities() {
@@ -14,27 +17,16 @@ function returnCities() {
     }
 };
 
-var getWeather = function (cityName) {
-    var apiURL = "https://api.openweather.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
-  
-    fetch(apiURL)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            displayWeather(data, cityName);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to Weather');
-      });
+var getWeather = function (event) {
+  //event.preventDefault();
+  var searchedCity = document.querySelector("#citySearchInput");
+  var city = searchedCity.value;
+    dailyWeather(city);
   };
 
 //localStorage.setItem(savedCity, cityName)
 
-var showForcast = function (weather, cityWeather) {
+var displayWeather = function (weather, cityWeather) {
     if (weather.length === 0) {
         citySearchReturn.textContent = "No Weather Data Available";
         return;
@@ -44,16 +36,67 @@ var showForcast = function (weather, cityWeather) {
     for (var i = 0; i < weather.length; i++ ) {
         var weatherDay = weather[i].data
         var forcastDay = document.createElement("div");
-        forcastDay.classList = "";
-        forcastDay.setAttribute("id", );
+        forcastDay.classList = "list-item flex-row align-center justify-space-between";
+        //forcastDay.setAttribute("id", );
+        forcastDay.textContent = weatherDay;
+
 
     }
 }
 
+function dailyWeather(city){
+  if (cityArray.indexOf(city)===-1){
+    cityArray.push(city);
+    renderCities();
+  }
+  var apiURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=" + apiKey
+  
+    fetch(apiURL)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            console.log(data);
+            console.log(data.name);
+            console.log(data.weather[0]);
+            console.log(data.main.feels_like);
+            // displayWeather(data, cityName);
+            // console.log(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to Weather');
+      });
 
-citySearchButton.addEventListener("submit", getWeather())
+}
+
+function renderCities() {
+  var citySearch = JSON.stringify(cityArray);
+  localStorage.setItem("history", citySearch);
+  cityList.innerHTML = "";
+  for (var i = 0; i < cityArray.length; i++) {
+  var previousCities = document.createElement("li");
+  previousCities.textContent = cityArray[i];
+  previousCities.addEventListener("click", function(event){
+    var city = event.target.innerText;
+    dailyWeather(city);
+  })
+  cityList.append(previousCities);
+  }
+
+
+}
+// var namedCity = data.name;
+// var cityWeather = data.main
+
+
+
+citySearchButton.addEventListener("click", getWeather)
 
 
 //var savedCities = document.createElement("button");
 
 
+renderCities();
